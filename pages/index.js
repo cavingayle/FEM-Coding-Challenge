4d0cadaa1b988e16303b1d301b0caee4a99ddabe
Cavin Gayle <cavingayle@gmail.com>
2021-01-27 21:58:09 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,56 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="py-10 px-4">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+      <div className="overflow-hidden rounded-lg mx-auto md:w-2/4">
+        <section className="bg-white p-8">
+          <h1 className="text-xl text-blue-300 font-semibold capitalize">
+            join our community
+          </h1>
+          <h2 className="text-yellow-600 font-semibold pt-4">
+            30-day hassel-free monye back guarantee
+          </h2>
+          <p className="pt-2 text-gray-500">
+            Gain access to our library tutorials along with eexpert code
+            reviews. Perfect for any developers who are serious about honing
+            theri skills
+          </p>
+        </section>
+        <section className="text-white md:flex md:flex-row">
+          <div className="bg-blue-300 p-8 md:w-1/2">
+            <h2 className="text-lg font-semibold">Money Subscription</h2>
+            <div className="flex items-center pt-2">
+              <p className="text-2xl font-semibold">$29</p>
+              <p className="ml-2 font-hairline ">per month</p>
+            </div>
+            <p className="font-light text-sm">
+              Full access for less than $1 day
             </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+            <button className="bg-yellow-600 hover:bg-blue-700 w-full text-white font-blod py-2 px-4 rounded mt-6 shadow-md">
+              Button
+            </button>
+          </div>
+          <div className="bg-blue-300 p-8 md:w-1/2">
+            <h2 className="font-semibold text-lg">Why Us</h2>
+            <div className="text-xs font-light pt-2">
+              <p>Tutorials by industry experts</p>
+              <p>Coding exercises Access</p>
+              <p>Peer & expert code review</p>
+              <p>Acces our Github Repo</p>
+              <p>Community forum</p>
+              <p>Flashcard decks</p>
+              <p>New cides every week</p>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        yellow: {
+          600: "#c0df34",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
